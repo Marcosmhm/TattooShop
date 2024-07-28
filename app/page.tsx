f80342c39aaa2ec7bdfc33f8dcc88aac9be7f02a
Marcos Henrique <mh.moraes@hotmail.com>
2024-07-28 12:14:02 -0300

    feat: implement animations in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
+"use client";
+
 import Image from "next/image";
+import { LazyMotion, m, domAnimation } from "framer-motion";
+
+const fadeInAnimationsVariants = {
+  initial: {
+    opacity: 0,
+    x: -100,
+  },
+};
 
 export default function Home() {
   return (
@@ -24,13 +34,48 @@ export default function Home() {
       <div className="hidden lg:block absolute left-20 -top-12 font-londrina uppercase font-normal text-text-muted opacity-15 text-[270px] tracking-wide z-0">
         Tattoo
       </div>
-      <div>
-        <h1 className="relative uppercase z-20 text-text-muted font-heading pt-[150px] pl-10 text-5xl leading-[1] break-keep lg:text-7xl lg:pl-[140px]">
-          <span>Inspire-se</span>
-          <span className="block">Expresse-se</span>
-          <span className="text-yellow block">Tatue-se</span>
-        </h1>
-      </div>
+      <LazyMotion features={domAnimation}>
+        <div>
+          <h1 className="relative uppercase z-20 text-text-muted font-heading pt-[150px] pl-10 text-5xl leading-[1] break-keep lg:text-7xl lg:pl-[140px]">
+            <m.span
+              variants={fadeInAnimationsVariants}
+              initial="initial"
+              animate={{
+                opacity: 1,
+                x: 0,
+                transition: { delay: 1, duration: 0.5 },
+              }}
+              className="block"
+            >
+              Inspire-se
+            </m.span>
+            <m.span
+              variants={fadeInAnimationsVariants}
+              initial="initial"
+              animate={{
+                opacity: 1,
+                x: 0,
+                transition: { delay: 1.5, duration: 0.5 },
+              }}
+              className="block"
+            >
+              Expresse-se
+            </m.span>
+            <m.span
+              variants={fadeInAnimationsVariants}
+              initial="initial"
+              animate={{
+                opacity: 1,
+                x: 0,
+                transition: { delay: 2, duration: 0.5 },
+              }}
+              className="text-yellow block"
+            >
+              Tatue-se
+            </m.span>
+          </h1>
+        </div>
+      </LazyMotion>
     </section>
   );
 }
